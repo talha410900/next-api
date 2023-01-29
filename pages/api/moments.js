8f96120074be1ab2b51ad36d10c0a8671ba76f9c
talha410900 <appledefender@gmail.com>
2023-01-29 22:48:32 +0500

    changes

--- pages/api/moments.js
+++ pages/api/moments.js
@@ -2,11 +2,16 @@ import NextCors from 'nextjs-cors';
 import example from "./exampleJSON.json";
 
 export default async function handler(req, res) {
+
   await NextCors(req, res, {
     // Options
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
     origin: '*',
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   });
+
+  const { id } = req.query
+
+
   res.status(200).json(example)
 }
--- public/index.js
+++ public/index.js
@@ -7,16 +7,16 @@ const css = `
     padding: 0 20px;
     width: 100%; 
     display: grid;
-    /* Define Auto Row size */
-    grid-auto-rows: 400px; 
-    /*Define our columns */
     grid-template-columns: repeat(auto-fill, minmax(300px, 1fr)); 
     grid-gap: 1em;
+    font-family: 'Poppins', sans-serif !important;
   }
 
 .urbnups_moment {
     width: 315px;
     text-align: left;
+    font-family: 'Poppins', sans-serif !important;
+
 }
 
 .urbnups_moment_image img {
@@ -29,6 +29,7 @@ const css = `
     position: absolute;
     margin-left: 10px;
     margin-top: 10px;
+    font-family: 'Poppins', sans-serif !important;
 
 }
 
@@ -44,13 +45,16 @@ const css = `
     text-decoration: none;
     font-size: 12px;
     font-weight: 600;
+    font-family: 'Poppins', sans-serif !important;
 
 
 }
 
 .urbnups_moment_creator a div {
     align-items: center;
-    display: flex
+    display: flex;
+    font-family: 'Poppins', sans-serif !important;
+
 }
 
 .urbnups_moment_creator a div span {
@@ -60,79 +64,89 @@ const css = `
 .urbnups_moment_title {
     font-size: 14px;
     font-weight: 600;
-
+    font-family: 'Poppins', sans-serif !important;
 
 }
 
 .urbnups_moment_description {
     font-size: 11px;
     font-weight: 300;
+    font-family: 'Poppins', sans-serif !important;
+
 }
 
 .urbnups_moment_favourite {
     position: absolute;
     margin-left: 265px;
-    margin-top: -25px
+    margin-top: -25px;
+    font-family: 'Poppins', sans-serif !important;
+
 }
 
 .urbnups_moment_favourite img {
     width: 40px;
     height: 40px;
     object-fit: contain;
-}`
+}
 
-async function getData() {
-    let data = await fetch("https://next-api-psi-roan.vercel.app/api/moments")
+.lds-ring {
+    display: inline-block;
+    position: relative;
+    width: 80px;
+    height: 80px;
+  }
+  .lds-ring div {
+    box-sizing: border-box;
+    display: block;
+    position: absolute;
+    width: 64px;
+    height: 64px;
+    margin: 8px;
+    border: 8px solid #000;
+    border-radius: 50%;
+    animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+    border-color: #000 transparent transparent transparent;
+  }
+  .lds-ring div:nth-child(1) {
+    animation-delay: -0.45s;
+  }
+  .lds-ring div:nth-child(2) {
+    animation-delay: -0.3s;
+  }
+  .lds-ring div:nth-child(3) {
+    animation-delay: -0.15s;
+  }
+  @keyframes lds-ring {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+  .spinner{
+    height:100px;
+    width:100%;
+    display:flex;
+    align-items:center;
+    justify-content:center;
+  }
+`
+
+async function getData(id) {
+    let data = await fetch(`https://next-api-psi-roan.vercel.app/api/moments?widgetId=${id}`)
     return data.json();
 }
 
+
 window.addEventListener('DOMContentLoaded', async (event) => {
 
-    const arry = await getData()
+    const spinner = document.createElement("div")
+    spinner.setAttribute('class', 'spinner')
 
-    let getEle = document.getElementsByTagName("urbnups-moments-widget");
-    getEle = getEle[0];
+    spinner.innerHTML = '<div class="lds-ring"><div></div><div></div><div></div><div></div></div>'
 
-    // console.log(getEle.getAttribute("widget-id"))
-
-    const main = document.createElement("div")
-    main.setAttribute("id", 'urbnups-moments-widget');
-
-
-    for (let i = 0; i < arry.length; i++) {
-        const data = arry[i];
-        var html = `
-   
-        <div class="urbnups_moment_creator">
-            <a href=${data.deeplink} target='_blank'>
-            <div>
-            <img  src="${data.userPreviewImage}" >
-            <span> ${data.displayName}</span>
-            </div>
-            </a>
-        </div>
-         <div class="urbnups_moment_image">
-         <a href=${data.deeplink} target='_blank'>
-         <img src=${data.previewImage} width=315px />
-         </a>
-         </div>
-         <div class="urbnups_moment_favourite">
-         <img src="https://urbnups.com/wp-content/uploads/2022/10/bucketlisticon.png">
-         </div>
-         <div class="urbnups_moment_title">
-         <span>${data.name}</span>
-         </div>
-        <div class="urbnups_moment_description">
-         <span>${data.description_snippet}</span>
-         </div>
-    `;
-
-        const newdiv = document.createElement('div');
-        newdiv.setAttribute('class', "urbnups_moment");
-        newdiv.innerHTML = html
-        main.appendChild(newdiv)
-
-    }
+    document.getElementsByTagName("body")[0].insertAdjacentElement("afterend", spinner)
 
     var styleNode = document.createElement('style');
     styleNode.type = "text/css";
@@ -145,8 +159,82 @@ window.addEventListener('DOMContentLoaded', async (event) => {
     document.getElementsByTagName('head')[0].appendChild(styleNode);
 
 
+    let getEle = document.getElementsByTagName("urbnups-moments-widget");
+    getEle = getEle[0];
 
-    getEle.insertAdjacentElement("afterend", main);
-    getEle.remove()
+    const widgetId = getEle.getAttribute("widget-id")
+
+    const main = document.createElement("div")
+    main.setAttribute("id", 'urbnups-moments-widget');
+
+    const arry = await getData(widgetId);
+
+
+
+
+    if (arry) {
+
+
+
+        for (let i = 0; i < arry.length; i++) {
+            const data = arry[i];
+            var html = `
+       
+            <div class="urbnups_moment_creator">
+                <a href=${data.deeplink} target='_blank'>
+                <div>
+                <img  src="${data.userPreviewImage}" >
+                <span> ${data.displayName}</span>
+                </div>
+                </a>
+            </div>
+             <div class="urbnups_moment_image">
+             <a href=${data.deeplink} target='_blank'>
+             <img src=${data.previewImage} width=315px />
+             </a>
+             </div>
+             <div class="urbnups_moment_favourite">
+             <img src="https://urbnups.com/wp-content/uploads/2022/10/bucketlisticon.png">
+             </div>
+             <div class="urbnups_moment_title">
+             <span>${data.name}</span>
+             </div>
+            <div class="urbnups_moment_description">
+             <span>${data.description_snippet}</span>
+             </div>
+        `;
+
+            const newdiv = document.createElement('div');
+            newdiv.setAttribute('class', "urbnups_moment");
+            newdiv.innerHTML = html
+            main.appendChild(newdiv)
+        }
+
+
+        var googleApi = document.createElement('googleApi');
+        googleApi.type = 'text/css';
+        document.head.appendChild(googleApi);
+        googleApi.href = "https://fonts.googleapis.com";
+
+        var preconnect = document.createElement('link');
+        document.head.appendChild(preconnect);
+        preconnect.href = "https://fonts.gstatic.com";
+        preconnect.rel = 'stylesheet'
+        preconnect.origin = 'true'
+
+        var preconnect = document.createElement('link');
+        document.head.appendChild(preconnect);
+        preconnect.href = "https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap";
+        preconnect.rel = 'stylesheet'
+
+        getEle.insertAdjacentElement("afterend", main);
+        getEle.remove()
+
+        spinner.remove()
+    } else {
+        spinner.remove()
+
+    }
+
 
 });
